643e7adbcf0538e61d2a93affcb8371f217c752c
qianyi-gogogo <qianyigogo@naver.com>
2026-08-24 19:20:08 +0800

    Initialize Spring Boot project and add health API

---
+++ src/main/java/com/qianyi/sportstrainingassistant/SportsTrainingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.qianyi.sportstrainingassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTrainingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsTrainingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qianyi/sportstrainingassistant/SportsTrainingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qianyi.sportstrainingassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsTrainingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
